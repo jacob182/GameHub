f18c10bd6085274793fbf1447ca98c8d95b3820b
jacob182 <jacobcoorey@gmail.com>
2017-06-05 12:56:04 +1000

    redirect test

--- GameHub/controller/registration_process.php
+++ GameHub/controller/registration_process.php
@@ -20,33 +20,33 @@
 if (empty($email) || empty($username) || empty($password))
 {
 	$_SESSION['error'] = 'All fields are required!';
-	header("location:../view/signup.php");
+	header("location:/view/signup.php");
 	exit();
 }
 elseif (strlen($password) < 7)
 	{
 		$_SESSION['error'] = 'Password must be 7 characters or more.';
-		header("location:../view/signup.php");
+		header("location:/view/signup.php");
 		exit();
 	}
 
 elseif ($email != $confirmemail)
 {
 	$_SESSION['error'] = 'Please enter the same email address.';
-	header("location:../view/signup.php");
+	header("location:/view/signup.php");
 	exit();
 }
 
 elseif ($password != $confirmpassword)
 {
 $_SESSION['error'] = 'Please enter the same password.';
-header("location:../view/signup.php");
+header("location:/view/signup.php");
 exit();
 }
 elseif (!filter_var($email, FILTER_VALIDATE_EMAIL))
   {
   	$_SESSION['error'] = 'Please enter a valid email address.';
-  	header("location:../view/signup.php");
+  	header("location:/view/signup.php");
     exit();
   }
 
@@ -55,7 +55,7 @@ $count = count_username($username);
     if($count > 0)
   	{
   		$_SESSION['error'] = 'Username taken. Please retry.';
-  		header("location:../view/signup.php");
+  		header("location:/view/signup.php");
   		exit();
   	}
 				$password = passHash($password);
@@ -69,7 +69,7 @@ $count = count_username($username);
     		//create a success message
     		$_SESSION['success'] = 'Thank you for creating an account. Please login.';
     		//redirect to products.php
-    		header('location:../view/login.php');
+    		header('location:/view/login.php');
     	}
     	else
     	{
@@ -77,6 +77,6 @@ $count = count_username($username);
     		$_SESSION['error'] = 'An error has occurred. Please try again.';
 				var_dump($result);
     		//redirect to signup.php
-    		header('location:../view/signup.php');
+    		header('location:/view/signup.php');
     	}
     ?>
