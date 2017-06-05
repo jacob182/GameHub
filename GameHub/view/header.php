d18f53c0e4c5edc47aa28aeec603fcdb0a97c58f
jacob182 <jacobcoorey@gmail.com>
2017-06-05 14:15:44 +1000

    lets go

--- GameHub/view/header.php
+++ GameHub/view/header.php
@@ -14,6 +14,7 @@
     </noscript>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js"></script>
     <script type="text/javascript" src="../js/scripts.js"></script>
     <script type="text/javascript" src="../js/jquery.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/gridstack.js/0.2.6/gridstack.min.css" />
